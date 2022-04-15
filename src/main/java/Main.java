36e070a10910fb6475f7de15528bdc24b799027a
r.yurkin <freeday_it2k17@mail.ru>
2022-04-15 12:17:09 +0300

    created function bubble-sorting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
 public class Main {
 
-    public static void main(String args[])
-    {
-        int arr[] = {3,-1,-4,1,5,9,-2,6,-5,3,-5};
+    public static void main(String args[]) {
+        int arr[] = {3, -1, -4, 1, 5, 9, -2, 6, -5, 3, -5};
         int n = 11;
+        bubble(arr, n);
         System.out.print(arr);
+    }
+
+    /// bubble-sorting
+    public static void bubble(int arr[], int n) {
+        if (n <= 1)
+            return;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < i; j++)
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
     }
 }
