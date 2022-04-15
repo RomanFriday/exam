1f72f2b8b799ac0a72f0dfb5a4a21fd7fe3f092e
r.yurkin <freeday_it2k17@mail.ru>
2022-04-15 12:08:49 +0300

    Created new project

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String args[])
+    {
+        int arr[] = {3,-1,-4,1,5,9,-2,6,-5,3,-5};
+        int n = 11;
+        System.out.print(arr);
+    }
+}
