47ce4e838982ddaaffd5e0c67d0757724ee57389
r.yurkin <freeday_it2k17@mail.ru>
2022-04-15 12:40:52 +0300

    Create bubble-sorting tests

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    @Test
+    public void emptyTest() {
+        int arr[]={};
+        int n=0;
+        Main.bubble(arr,n);
+    }
+
+    @Test
+    public void oneElemTest(){
+        int arr[]={1};
+        int n=1;
+        Main.bubble(arr,n);
+        assertEquals(1,arr[0]);
+    }
+
+    @Test
+    public void twoElemTest(){
+        int arr[]={0,-1};
+        int n=2;
+        Main.bubble(arr,n);
+        int exp[]={-1,0};
+        assertArrayEquals(exp,arr);
+    }
+
+    @Test
+    public void equalsElemTest(){
+        int arr[]={-3,-3,-3};
+        int n=3;
+        Main.bubble(arr,n);
+        int exp[]={-3,-3,-3};
+        assertArrayEquals(exp,arr);
+    }
+
+    @Test
+    public void differentElemsTest(){
+        int arr[]={3,-1,0,5,0,3,-2};
+        int n=7;
+        Main.bubble(arr,n);
+        int exp[]={-2,-1,0,0,3,3,5};
+        assertArrayEquals(exp,arr);
+    }
+
+}
